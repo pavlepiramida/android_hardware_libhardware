4cd4699bbd9fa76a84785593958a89f8af63f717
klinkone <klinkone@gmail.com>
2015-11-29 18:37:58 +0100

    libhardware: audio.h build error patch
    Change-Id: I9b6af01764821c97ed1319059b2d7e9a8d6a747d

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -148,7 +148,7 @@ __BEGIN_DECLS
 #define AUDIO_OFFLOAD_CODEC_WMA_BIT_PER_SAMPLE "music_offload_wma_bit_per_sample"
 #define AUDIO_OFFLOAD_CODEC_WMA_CHANNEL_MASK "music_offload_wma_channel_mask"
 #define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION "music_offload_wma_encode_option"
-#define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION1"music_offload_wma_encode_option1"
+#define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION1 "music_offload_wma_encode_option1"
 #define AUDIO_OFFLOAD_CODEC_WMA_ENCODE_OPTION2 "music_offload_wma_encode_option2"
 #define AUDIO_OFFLOAD_CODEC_FORMAT  "music_offload_codec_format"
 #define AUDIO_OFFLOAD_CODEC_FLAC_MIN_BLK_SIZE "music_offload_flac_min_blk_size"
@@ -563,6 +563,9 @@ static inline size_t audio_stream_frame_size(const struct audio_stream *s)
         }
         return popcount(chan_mask) * chan_samp_sz;
     }
+
+    return 0;
+
 #else
     audio_format_t format = s->get_format(s);
 
